031874aea48b46db456e5633839b42c6074d3634
ttungbmt <ttungbmt@gmail.com>
2024-08-07 15:16:46 +0700

    Update changes

--- layers/wld_cities_pt/wld_cities_pt.sql
+++ layers/wld_cities_pt/wld_cities_pt.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION layer_wld_cities_pt(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, city_name text, status text, city_name_vi text, cntry_name text, level int) AS $$
-    SELECT geometry, city_name, status, city_name_vi, cntry_name, level
+RETURNS TABLE(geometry geometry, city_name text, status text, name_vi text, cntry_name text, level int) AS $$
+    SELECT geometry, city_name, status, name_vi, cntry_name, level
     FROM wld_cities_pt
     WHERE geometry && bbox
       AND zoom_level >= 5;
