a9049aac8aefed63e657a8eb22ee70e88ea3420a
nhan.tt <nhan.tt@ots.vn>
2026-01-14 10:37:51 +0700

    Fix rendering bugs

--- layers/wld_cities_pt/wld_cities_pt.sql
+++ layers/wld_cities_pt/wld_cities_pt.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION layer_wld_cities_pt(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, city_name text, status text, name_vi text, cntry_name text, level int) AS $$
-    SELECT geometry, city_name, status, name_vi, cntry_name, level
+RETURNS TABLE(geometry geometry, city_name text, status text, name_vi text, cntry_name text) AS $$
+    SELECT geometry, city_name, status, name_vi, cntry_name
     FROM wld_cities_pt
-    WHERE geometry && bbox
-      AND zoom_level >= 5;
+    WHERE zoom_level >= 5;
+      -- AND geometry && bbox;
 $$ LANGUAGE SQL IMMUTABLE;
