9e5a8716f746cac3688f1cb2ee4bb9ec9e7b3012
nhan.tt <nhan.tt@ots.vn>
2026-05-22 14:03:05 +0700

    Remove temp admin layers

--- layers/vn_wards_pt_v1/vn_wards_pt_v1.sql
+++
@@ -1,9 +0,0 @@
-DROP FUNCTION IF EXISTS layer_vn_wards_pt_v1;
-
-CREATE OR REPLACE FUNCTION layer_vn_wards_pt_v1(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, code text, name text, name_en text, level text, admin_level int, label text, rank int) AS $$
-    SELECT geometry, code, name, name_en, level, admin_level, label, rank
-    FROM vn_wards_pt_v1
-    WHERE geometry && bbox
-            AND zoom_level >= 3;
-$$ LANGUAGE SQL IMMUTABLE;
