6f5f7c4217d982a50ed528e6ffaa42acc303f5c3
nhan.tt <nhan.tt@ots.vn>
2026-09-04 14:37:51 +0700

    Remove unused layers

--- layers/vn_boundary_mask/vn_boundary_mask.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION layer_vn_boundary_mask(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry) AS $$
-    SELECT geometry
-    FROM vn_boundary_mask
-    WHERE geometry && bbox;
-$$ LANGUAGE SQL IMMUTABLE;
--- layers/wld_cities_pt/wld_cities_pt.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION layer_wld_cities_pt(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, city_name text, status text, name_vi text, cntry_name text) AS $$
-    SELECT geometry, city_name, status, name_vi, cntry_name
-    FROM wld_cities_pt
-    WHERE zoom_level >= 5;
-      -- AND geometry && bbox;
-$$ LANGUAGE SQL IMMUTABLE;
